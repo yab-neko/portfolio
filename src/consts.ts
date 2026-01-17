8f036d19216b556eeaf604335e99e8a7a1600509
yab-neko <works@yab-neko.com>
2026-01-17 17:40:50 +0900

    fix title text

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Art Portfolio - Art Void';
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'FUKUHARA Miwa illustration';
+export const SITE_DESCRIPTION = 'イラストレーター フクハラミワのポートフォリオサイト'
